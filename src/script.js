66099ec5b1a6152c0275c3b7e1495f76b707bca1
reb84 <152401972+reb84@users.noreply.github.com>
2023-12-07 19:49:16 +0000

    added script for current date

---
+++ src/script.js
@@ -0,0 +1,43 @@
+//// DATE + TIME ////
+
+function formatDate(date) {
+  let currentDay = date.getDay();
+  let currentDate = date.getDate();
+  let currentMonth = date.getMonth();
+  let currentHour = date.getHours();
+  let currentMinute = date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let formattedDay = days[currentDay];
+  let formattedMonth = months[currentMonth];
+
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+
+  return `${formattedDay}  -   ${currentDate} ${formattedMonth}  -  ${currentHour}:${currentMinute}`;
+}
+
+let currentLocalDate = document.querySelector("#current-date");
+let localDate = new Date();
+currentLocalDate.innerHTML = formatDate(localDate);
+
+////  ////
